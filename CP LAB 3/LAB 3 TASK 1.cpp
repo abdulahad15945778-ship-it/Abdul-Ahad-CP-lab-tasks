1028e8db9f2ab0a35417b550ef3ffc05f385e338
Abdul Ahad <abdul15945778@gmail.com>
2026-03-02 23:28:18 +0500

    Update LAB 3 TASK 1.cpp

--- CP LAB 3/LAB 3 TASK 1.cpp
+++ CP LAB 3/LAB 3 TASK 1.cpp
@@ -1,4 +1,4 @@
-
+// name : abdulahad ; etea :5124772
 #include <iostream>
 using namespace std;
 int main()
@@ -11,16 +11,17 @@ int main()
     	cout<<"Enter Second Number :\n";  //OUTPUT
     	cin>>num2;
 
-if (num1< num2){  //IF USED FOR COMPARISON BTW TWO NUMBERS
+if (num1< num2){  //IF USED FOR COMPARISON BTW TWO NUMBERS   Agr pehla number chota ho or dusra bara ho to if ki condition show ho gi 
 	cout<<num1<<"  is smaller than  "<<num2<<endl;
 }
 
-else if (num1>num2){   //ELSE IF USED FOR OTHER THAN IF CONDITION
+else if (num1>num2){   //ELSE IF USED FOR OTHER THAN IF CONDITION  agr pehla number bara ho or dusra chota ho ti ifelse ki body show ho gi
 	cout<<num1<<"  is greater than  "<<num2<<endl;
 }
-else {  // USED IF THE FORST CONDITION DEOS NOT SATISFY
+else {  // USED IF THE FORST CONDITION DEOS NOT SATISFY agr dono equal ho to ya condition ki body show ho gi
 	cout<<num1<<"  is equal to  "<<num2;
 }
 
 return 0;
 }
+
