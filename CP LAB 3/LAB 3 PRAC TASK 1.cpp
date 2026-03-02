ee96dc0ee8b96f4bc55ff511db50e0d5d28e47f5
Abdul Ahad <abdul15945778@gmail.com>
2026-03-02 23:21:44 +0500

    Update LAB 3 PRAC TASK 1.cpp

--- CP LAB 3/LAB 3 PRAC TASK 1.cpp
+++ CP LAB 3/LAB 3 PRAC TASK 1.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
+#include <iostream>// name : abdulahad ; etea :5124772
 using namespace std;
 
 int main(){
-	int age;  // DECLARATION OF VARIABLE
+	int age;  // DECLARATION OF VARIABLE integer
 	cout << "Enter your age:";    // DISPALY TO USER
 	cin >> age;  //INPUT K LIYE
-	if(age >=18){     //CONDITION DENY K LIYE
+	if(age >=18){     //CONDITION ha true hui to if ki body show ho gi nhi to else ki body 
 		cout << "You are elligible to vote." <<endl; // DISPLAY
 	}
-	else {  // ELSE CONDITION DENY K LIYE
+	else {  // ELSE CONDITION 
 		cout << "You are not elligible to vote." <<endl; //DISPLAY
 	}
 	return 0;
+
 }
