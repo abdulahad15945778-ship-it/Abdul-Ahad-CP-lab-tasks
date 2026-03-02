e137a3592743246db574f52ffc243d9a02eb7e79
Abdul Ahad <abdul15945778@gmail.com>
2026-03-02 23:33:57 +0500

    Update LAB 3 TASK 2.cpp

--- CP LAB 3/LAB 3 TASK 2.cpp
+++ CP LAB 3/LAB 3 TASK 2.cpp
@@ -1,4 +1,4 @@
-
+// name : abdulahad ; etea :5124772
 #include <iostream>
 using namespace std;
 int main()
@@ -8,13 +8,12 @@ cout<<"enter a number"<<endl;// DISPLY K LYE
 cin>>num;// USER SA INOUT LAINY K LYE 
 
 
-if ( num>0 && num<100 )// KOI B CONDITION LAGANY K LYE 
-
-
+if ( num>0 && num<100 )// KOI B CONDITION LAGANY K LYE agr 0 sa 100 ka darmiyan no ho to if ki body show ho gi
 cout<<num<<" is in range of 0 to 100 "<<endl;
 
-else   // IF WALI CONDITION AGR SATISFY NA HO TU ELSE LGYEN GY 
+else   // IF WALI CONDITION AGR SATISFY NA HO TU ELSE LGYEN GY agr 0 sa 100 ka darmiyan na ho to else wali body show ho gi
 cout<<num<< " is not in range of 0 to 100";
 
 return 0;
 }
+
