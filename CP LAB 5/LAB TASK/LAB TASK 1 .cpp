fbb96f7b14e18324da4e7ad945cd2311655e5b6f
Abdul Ahad <abdul15945778@gmail.com>
2026-03-09 20:11:07 +0500

    Update LAB TASK 1 .cpp

--- CP LAB 5/LAB TASK/LAB TASK 1 .cpp
+++ CP LAB 5/LAB TASK/LAB TASK 1 .cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-using namespace std;
+using namespace std; 
 int main()
 {
-	int q;
+	int q; // q ko decelare kiya ha
 	do{
-		cout<<q<<endl;
-		q++;		
+		cout<<q<<endl; //q ki value display ho gi
+		q++;		// q ki value increment ho gi
 	}
-	while(q<=10);
+	while(q<=10);// ya tab tag q ki value zayada ho gi jab tak q ki value 10 ka equal na ho jaya
 	{
 		return 0;
 	}
+
 }
