56ca8699a9288084c73222e26f94e5ddfd5a87c8
Abdul Ahad <abdul15945778@gmail.com>
2026-03-09 20:35:36 +0500

    Add files via upload

---
+++ CP LAB 5/LAB TASK/LAB TASK 1 .2.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+int q; // q decelare kiya ha
+while(q<=10){// while ma condition lagai ha
+
+	cout<< q<<endl;// q cout ho ga user ko show ho ga
+		q ++; // qki value increment ho gi
+}
+
+
+return 0;
+}
